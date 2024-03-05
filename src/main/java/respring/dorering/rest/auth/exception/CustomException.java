aa6a2b7ec6465fad6f8426a0092303506130bca3
jiyeong08 <a01086283142@gmail.com>
2024-03-05 11:59:55 +0900

    Revert "first"
    This reverts commit d272a148d18a15c8b17060afd180465d002699c5.

--- src/main/java/respring/dorering/rest/auth/exception/CustomException.java
+++
@@ -1,8 +0,0 @@
-package respring.dorering.rest.auth.exception;
-
-// 사용자 정의 예외 클래스
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
