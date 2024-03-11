7fa7210eb189ee23fcf0b42d1520b55ec21153eb
bockson <148856910+bockson@users.noreply.github.com>
2024-03-11 12:40:41 +0900

    [Feature] 상세 페이지 구현 중

---
+++ src/main/java/respring/dorering/rest/fairytale/entity/Detail.java
@@ -0,0 +1,13 @@
+//package respring.dorering.rest.fairytale.entity;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "fairytale_detail")
+//public class Detail {
+//
+//
+//
+//}
---
+++ src/main/java/respring/dorering/rest/fairytale/repository/DetailRepository.java
@@ -0,0 +1,8 @@
+package respring.dorering.rest.fairytale.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Spring Data JPA를 사용하여 Detail 엔티티에 대한 저장소를 정의합니다.
+// DetailRepository.java
+public interface DetailRepository{
+}
